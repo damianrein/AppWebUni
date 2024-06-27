4deb32018c040afe3b16c0582a306f8ce2998d18
damianrein <damianrein464@gmail.com>
2024-06-27 02:06:11 -0300

    -

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Home',component:HomeComponent},
+  {path:'login',component:LoginPageComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
