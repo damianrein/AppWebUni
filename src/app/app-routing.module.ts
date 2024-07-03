cd65b750a69f35fec2932177751ffdce138e6241
damianrein <damianrein464@gmail.com>
2024-07-02 21:46:33 -0300

    -

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { InvestigationsComponent } from './components/investigations/investigations.component';
 
 const routes: Routes = [
   {path:'Home',component:HomeComponent},
   {path:'login',component:LoginPageComponent},
   {path:'register',component:RegisterPageComponent},
+  {path:'investigations',component:InvestigationsComponent},
   {path:'',redirectTo:'home',pathMatch:'full'}
 ];
 
