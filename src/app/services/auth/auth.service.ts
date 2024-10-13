8dee3c3dc9b288ddc0c614d791c8abd032c446e0
damianrein <damianrein464@gmail.com>
2024-10-12 21:49:54 -0300

    -

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  apiUrl = "localhost:8080/auth";
+
+  constructor(private http: HttpClient) { }
+
+  authLogin(email: String, password: String):Observable<any>{
+    return this.http.post<any>('${this.apiUrl}', {email, password});
+  }
+
 }
